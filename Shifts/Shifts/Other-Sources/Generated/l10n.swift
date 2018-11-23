88b11fa78d92170a11b91e925673d3a2fdd282c4
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2018-11-24 10:56:28 +1100

    Added localisation and assets

---
+++ Shifts/Shifts/Other-Sources/Generated/l10n.swift
@@ -0,0 +1,34 @@
+// swiftlint:disable all
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Strings
+
+// swiftlint:disable function_parameter_count identifier_name line_length type_body_length
+internal enum L10n {
+  /// Shift
+  internal static let shift = L10n.tr("Localizable", "Shift")
+  /// Shifts
+  internal static let shifts = L10n.tr("Localizable", "Shifts")
+  /// Start
+  internal static let start = L10n.tr("Localizable", "Start")
+  /// Stop
+  internal static let stop = L10n.tr("Localizable", "Stop")
+}
+// swiftlint:enable function_parameter_count identifier_name line_length type_body_length
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
+
