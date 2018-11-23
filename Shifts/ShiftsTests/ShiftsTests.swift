363e9bf019fa03110f58fb30ea1c91f750f1fa31
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2018-11-24 10:47:13 +1100

    Added cocoapods configuration

---
+++ Shifts/ShiftsTests/ShiftsTests.swift
@@ -0,0 +1,33 @@
+//
+//  ShiftsTests.swift
+//  ShiftsTests
+//
+//  Created by Dmitry Klimkin on 24/11/18.
+//  Copyright © 2018 Dmitry Klimkin. All rights reserved.
+//
+
+import XCTest
+
+class ShiftsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
