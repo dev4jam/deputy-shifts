3f05d3ded418251a65df47f5fa3e72fc48e8a5fe
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2018-11-24 11:14:12 +1100

    Added unit-tests' templates, mocks and generation step

---
+++ Shifts/ShiftsTests/Generated/AutoMockable.generated.swift
@@ -0,0 +1,24 @@
+// Generated using Sourcery 0.15.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+// swiftlint:disable line_length
+// swiftlint:disable variable_name
+
+import Foundation
+import RIBs
+import RxSwift
+
+@testable import Shifts
+
+
+
+
+
+
+
+
+
+
+
+
+
